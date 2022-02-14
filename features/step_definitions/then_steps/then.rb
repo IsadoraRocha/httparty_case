ca8b791295d5da722e047515c1957506d820915a
Isadora R Paula <inmetrics.ipaula@hubfintech.com.br>
2022-02-14 10:23:49 -0300

    inclusão de comentários

--- features/step_definitions/then_steps/then.rb
+++ features/step_definitions/then_steps/then.rb
@@ -1,5 +1,5 @@
 Entao('valido que houve sucesso na consulta') do
   expect(@response.code).to eq(200)
   expect(@response).to match_json_schema('vr_schema')
-  puts JSON.pretty_generate(@response['typeOfEstablishment'][rand(0... @response['typeOfEstablishment'].length)])
+  puts JSON.pretty_generate(@response['typeOfEstablishment'][rand(0...@response['typeOfEstablishment'].length)])
 end
